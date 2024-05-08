197ff9f9408b8ae3bfe054d515cde15ca72432e8
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-08 16:03:39 +0300

    skeleton for home

--- app/[locale]/(authenticated)/page.tsx
+++ app/[locale]/(authenticated)/page.tsx
@@ -1,7 +1,58 @@
+import { H1, H6 } from "@/components/H";
+import TextInputField from "@/components/TextInputField";
+import { getI18n } from "@/locales/server";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <GlobalSearch />
+      <MyStats />
+      <Schedule />
+      <Announcements />
     </div>
   );
 }
+
+async function GlobalSearch() {
+  const t = await getI18n();
+  return (
+    <div>
+      <H1>{t("globalSearch")}</H1>
+      <TextInputField placeholder={t("search")} />
+    </div>
+  );
+}
+
+function MyStats() {
+  return (
+    <div>
+      <div>
+        <H6>My GPA</H6>
+        <p>4.0</p>
+      </div>
+      <div>
+        <H6>Completed Credit Hours</H6>
+        <p>120</p>
+      </div>
+    </div>
+  );
+}
+
+function Schedule() {
+  return (
+    <div>
+      <h1>Schedule</h1>
+
+    </div>
+  );
+}
+
+function Announcements() {
+  return (
+    <div>
+      <h1>Announcements</h1>
+    </div>
+  );
+}
+
+
