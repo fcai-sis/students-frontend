2a4cc11ff732c65a95fdd93a2b4d315bf80cf82d
Amr Khaled <lewolfje@gmail.com>
2024-06-28 12:26:09 +0300

    replace fake student data with real one in home page

--- app/[locale]/(authenticated)/page.tsx
+++ app/[locale]/(authenticated)/page.tsx
@@ -5,30 +5,36 @@ import ServiceRequestCard from "@/components/ServiceRequestCard";
 import { dummyCourses } from "@/dummy/courses";
 import dummySchedule from "@/dummy/schedule";
 import { dummySlotsByDay, dummyTimeRanges } from "@/dummy/slots";
-import { dummyStudents } from "@/dummy/students";
 import { fakeResponse, localizedLevel } from "@/dummy/utils";
-import { tt } from "@/lib";
+import { getAccessToken, tt } from "@/lib";
 import { getCurrentLocale, getI18n } from "@/locales/server";
 import { getStudentServiceRequests } from "./requests/page";
 import { getAnnouncements } from "./announcements/page";
+import { studentsAPI } from "@/api";
+import { revalidatePath } from "next/cache";
+
+const getStudent = async () => {
+  const accessToken = await getAccessToken();
+
+  const response = await studentsAPI.get(`/me`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (response.status !== 200) throw new Error("Failed to fetch student");
+
+  revalidatePath("/");
+
+  return response.data;
+};
 
 export default async function Page() {
   const t = await getI18n();
   const locale = getCurrentLocale();
 
-  const _me = dummyStudents[0];
-  const getMeResponse = await fakeResponse({
-    status: 200,
-    data: {
-      student: _me,
-    },
-  });
-
-  if (getMeResponse.status !== 200) {
-    throw new Error("Failed to fetch student data");
-  }
-
-  const { student } = getMeResponse.data;
+  const getStudentResponse = await getStudent();
+  const student = getStudentResponse.student;
 
   const { announcements } = await getAnnouncements(1);
   const { serviceRequests } = await getStudentServiceRequests(1);
@@ -58,27 +64,27 @@ export default async function Page() {
 
   return (
     <>
-      <div className="flex items-center justify-between pb-4 w-full">
-        <div className="flex flex-col gap-4">
-          <h2 className="flex gap-4 items-center">
+      <div className='flex items-center justify-between pb-4 w-full'>
+        <div className='flex flex-col gap-4'>
+          <h2 className='flex gap-4 items-center'>
             {t("home.greeting", {
               name: student.fullName.split(" ")[0],
             })}
           </h2>
-          <div className="flex gap-2">
-            <p className="rounded-lg bg-slate-100 text-slate-500 px-4 py-2">
+          <div className='flex gap-2'>
+            <p className='rounded-lg bg-slate-100 text-slate-500 px-4 py-2'>
               {student.studentId}
             </p>
-            <p className="rounded-lg bg-blue-100 text-blue-500 px-4 py-2">
+            <p className='rounded-lg bg-blue-100 text-blue-500 px-4 py-2'>
               {tt(locale, localizedLevel(student.level))}
             </p>
-            <p className="rounded-lg bg-blue-100 text-blue-500 px-4 py-2">
+            <p className='rounded-lg bg-blue-100 text-blue-500 px-4 py-2'>
               {tt(locale, student.major.name)}
             </p>
           </div>
         </div>
-        <div className="flex gap-2">
-          <div className="flex flex-col items-center">
+        <div className='flex gap-2'>
+          <div className='flex flex-col items-center'>
             <RadialProgress
               colorize={false}
               value={student.creditHours}
@@ -93,7 +99,7 @@ export default async function Page() {
               </b>
             </p>
           </div>
-          <div className="flex flex-col items-center">
+          <div className='flex flex-col items-center'>
             <RadialProgress colorize value={student.gpa} max={4} />
             <p>
               <b>
@@ -106,18 +112,18 @@ export default async function Page() {
           </div>
         </div>
       </div>
-      <div className="flex-col py-4 w-full">
+      <div className='flex-col py-4 w-full'>
         <h2>{t("home.schedule")}</h2>
         <Schedule slots={slots} timeRanges={timeRanges} schedule={schedule} />
       </div>
-      <div className="flex gap-4 py-4 w-full">
-        <div className="w-full">
+      <div className='flex gap-4 py-4 w-full'>
+        <div className='w-full'>
           <h2>{t("announcements.title")}</h2>
           {announcements.map((announcement, index) => (
             <AnnouncementCard key={index} announcement={announcement} />
           ))}
         </div>
-        <div className="w-full">
+        <div className='w-full'>
           <h2>{t("serviceRequests.title")}</h2>
           {serviceRequests.map((serviceRequest: any, i: number) => (
             <ServiceRequestCard key={i} serviceRequest={serviceRequest} />
